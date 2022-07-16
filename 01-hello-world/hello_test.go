750906bd0eeb63b29686ad39a243eb5031ca7490
Wes T4N <wesleyt1998@gmail.com>
2022-07-16 11:51:58 -0400

    refactor hello world

--- 01-hello-world/hello_test.go
+++ 01-hello-world/hello_test.go
@@ -2,11 +2,19 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello("Wes")
-	want := "Hello, Wes"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+func TestHello(t *testing.T) {
+    assertEq := func(t testing.TB, got string, want string) {
+        t.Helper()
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
+    }
+
+    t.Run("saying hello to peopl", func (t *testing.T) {
+        assertEq(t, Hello("Wes"), "Hello, Wes")
+    })
+    t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+        assertEq(t, Hello(""), "Hello, World")
+    })
 }
