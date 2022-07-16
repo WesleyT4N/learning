05b88d9ba8a3e514de5003adc0f0599a2133da05
Wes T4N <wesleyt1998@gmail.com>
2022-07-16 12:04:56 -0400

    final hello world refactor

--- 01-hello-world/hello.go
+++ 01-hello-world/hello.go
@@ -2,15 +2,31 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const engHelloPrefix = "Hello, "
+const spanHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
-    if name == "" {
-        name = "World"
+func greetingPrefix(language string) string {
+    switch language {
+        case spanish:
+            return spanHelloPrefix
+        case french:
+            return frenchHelloPrefix
+        default:
+            return engHelloPrefix
     }
-    return engHelloPrefix + name
 }
 
-func main()  {
-    fmt.Println(Hello("world"))
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
 }
--- 01-hello-world/hello_test.go
+++ 01-hello-world/hello_test.go
@@ -11,10 +11,16 @@ func TestHello(t *testing.T) {
         }
     }
 
-    t.Run("saying hello to peopl", func (t *testing.T) {
-        assertEq(t, Hello("Wes"), "Hello, Wes")
+    t.Run("saying hello to peopl", func(t *testing.T) {
+        assertEq(t, Hello("Wes", ""), "Hello, Wes")
     })
     t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-        assertEq(t, Hello(""), "Hello, World")
+        assertEq(t, Hello("", ""), "Hello, World")
+    })
+    t.Run("in Spanish", func(t *testing.T) {
+        assertEq(t, Hello("Elodie", "Spanish"), "Hola, Elodie")
+    })
+    t.Run("in French", func(t *testing.T) {
+        assertEq(t, Hello("Wes", "French"), "Bonjour, Wes")
     })
 }
