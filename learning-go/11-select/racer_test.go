b9f7c6356f4ed21b069e1ed0f63412d89ec5547e
Wes Tan <wes.tan@datadoghq.com>
2022-09-24 15:55:19 -0400

    add select lesson code

---
+++ learning-go/11-select/racer_test.go
@@ -0,0 +1,49 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+    t.Run("returns the faster url", func (t *testing.T) {
+        slowServer := makeDelayedServer(20 * time.Millisecond)
+        fastServer := makeDelayedServer(0)
+        defer slowServer.Close()
+        defer fastServer.Close()
+
+        slowURL := slowServer.URL
+        fastURL := fastServer.URL
+
+        want := fastURL
+        got, err := Racer(slowURL, fastURL)
+
+        if err != nil {
+            t.Fatalf("did not expect an error but got %v", err)
+        }
+
+
+        if got != want {
+            t.Errorf("got %q, want %q", got, want)
+        }
+    })
+
+    t.Run("returns an error if a server doesn't respond within specified time", func (t *testing.T) {
+        server := makeDelayedServer(10 * time.Millisecond)
+        defer server.Close()
+        _, err := ConfigurableRacer(server.URL, server.URL, 1 * time.Millisecond)
+        if err == nil {
+            t.Error("expected an error but didn't get one")
+        }
+    })
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+    return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        time.Sleep(delay)
+        w.WriteHeader(http.StatusOK)
+    }))
+}
+
